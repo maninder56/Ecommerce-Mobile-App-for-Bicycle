f6985667c02aae60dec974ed15e9c352f51d25db
singhm50 <singhm50@coventry.ac.uk>
2023-04-05 15:28:34 +0100

    ShortDescription added in bikes model

--- Shop Bike/Models/Bike.cs
+++ Shop Bike/Models/Bike.cs
@@ -9,6 +9,7 @@ namespace Shop_Bike.Models
     public class Bike
     {
         public string Name { get; set; }   
+        public string ShortDescription { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
         public double Price { get; set; }
