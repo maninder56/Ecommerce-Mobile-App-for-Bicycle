80a01b44a46afb14b1044828c2ce6272e1dd74e0
singhm50 <singhm50@coventry.ac.uk>
2023-04-05 17:03:56 +0100

    database for cart added

--- Shop Bike/Models/Cart.cs
+++ Shop Bike/Models/Cart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Shop_Bike.Models
 {
-    internal class Cart
+    public class Cart
     {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
     }
 }
