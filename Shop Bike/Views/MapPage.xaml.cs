d49228b36e023049571fa8592b175ff9d29573ad
singhm50 <singhm50@coventry.ac.uk>
2023-04-06 16:06:53 +0100

    open map button fixed

--- Shop Bike/Views/MapPage.xaml.cs
+++ Shop Bike/Views/MapPage.xaml.cs
@@ -12,26 +12,4 @@ public partial class MapPage : ContentPage
 		InitializeComponent();
 		BindingContext = map;
 	}
-
-    private async void Map_Button_Clicked(object sender, EventArgs e)
-    {
-        await OpenMapAsync();
-    }
-
-    public async Task OpenMapAsync()
-    {
-        var location = new Location(47.645160, -122.1306032);
-
-        try
-        {
-            await map.OpenAsync(location);
-        }
-        catch (Exception ex)
-        {
-            await Shell.Current.DisplayAlert("Error!",
-                $"Unable to open map: {ex.Message}", "OK");
-        }
-
-
-    }
 }
